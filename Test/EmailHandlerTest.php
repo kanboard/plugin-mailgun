05b6ac80605efa3db2b4aab58cce2c8bc127fc60
David Morlitz <david@morlitz.com>
2018-09-27 04:38:28 -0700

    Added unit tests - fixed PHP 5.6 project numbering in test - fixed some style issues

--- Test/EmailHandlerTest.php
+++ Test/EmailHandlerTest.php
@@ -91,10 +91,10 @@ class EmailHandlerTest extends Base
 
         $this->assertEquals(1, $projectModel->create(array('name' => 'test1')));
         $this->assertEquals(2, $projectModel->create(array('name' => 'test2', 'email' => 'test2@localhost')));
-        $this->assertEquals(4, $projectModel->create(array('name' => 'test4', 'email' => 'test4@localhost')));
+        $this->assertEquals(3, $projectModel->create(array('name' => 'test4', 'email' => 'test4@localhost')));
 
-        // Allow project 4 to receive E-Mail from any sender
-        $this->assertTrue($projectMetadataModel->save(4, array('MailgunProject_catchall' => 'me@localhost')));
+        // Allow project 3 to receive E-Mail from any sender
+        $this->assertTrue($projectMetadataModel->save(3, array('MailgunProject_catchall' => 'me@localhost')));
 
         // Empty payload
         $this->assertFalse($emailHandler->receiveEmail(array()));
